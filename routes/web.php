74dce12575230e21e8323fdbef1f14c9253a50c9
williamorvie <90943778+williamorvie@users.noreply.github.com>
2022-04-12 12:31:09 +0700

    OAuth

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
-Route::get('login', function () {
-    return view('login');
-})->name('login');
+
 Route::get('checkout', function () {
     return view('checkout');
 })->name('checkout');
-Route::get('success-checkout', function () {
-    return view('success_checkout');
-})->name('success-checkout');
+
+Route::get('succes-checkout', function () {
+    return view('succes-checkout');
+})->name('succes-checkout');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
