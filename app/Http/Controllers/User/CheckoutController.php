b8a09ca56d07b6fb50596b1356dfd83a4e95be50
williamorvie <90943778+williamorvie@users.noreply.github.com>
2022-05-13 21:13:09 +0700

    Finish

--- app/Http/Controllers/User/CheckoutController.php
+++ app/Http/Controllers/User/CheckoutController.php
@@ -71,6 +71,8 @@ class CheckoutController extends Controller
         $user->email = $data['email'];
         $user->name = $data['name'];
         $user->occupation = $data['occupation'];
+        $user->phone = $data['phone'];
+        $user->address = $data['address'];
         $user->save();
 
         // create checkout
--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -34,11 +34,12 @@
                             <strong>{{$checkout->Camp->price}}K</strong>
                         </td>
                         <td>
-                            @if ($checkout->is_paid)
-                                <strong class="text-success">Payment Success</strong>
-                            @else
-                                <strong>Waiting for Payment</strong>
-                            @endif
+                            <strong>{{$checkout->payment_status}}K</strong>
+                        </td>
+                        <td>
+                            @if ($checkout->payment_status == 'waiting')
+                                        <a href="{{$checkout->midtrans_url}}" class="btn btn-primary">Pay Here</a>
+                                    @endif
                         </td>
                         <td>
                             <a href="https://wa.me/082236739789?text=Hi, saya ingin bertanya tentang kelas {{ $checkout->Camp->title }}" class="btn btn-primary">
@@ -49,7 +50,7 @@
                     @empty
                         <tr>
                             <td colspan="5">
-                                <h3>No Data</h3>
+                                <h3>No Camp Registered</h3>
                             </td>
                         </tr>
                     @endforelse
